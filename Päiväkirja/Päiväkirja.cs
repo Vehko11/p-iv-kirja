a2add36e5bd7bb160fa4a1fe5dc2d3e15a1703d0
Vehko <shaltearbestgal777@gmail.com>
2025-08-27 17:20:35 +0300

    Update Päiväkirja.cs

--- Päiväkirja/Päiväkirja.cs
+++ Päiväkirja/Päiväkirja.cs
@@ -1,15 +1,12 @@
 using System;
 
-namespace Numbers
+public class HelloWorld
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int count = 0;
+        while (count < 1)
         {
-            //do
-            //
-            //{
-
             Console.WriteLine("valitse alta:");
 
             Console.WriteLine("1: uusi");
@@ -17,6 +14,8 @@ namespace Numbers
             Console.WriteLine("2: muokkaa");
 
             Console.WriteLine("3: lue");
+            
+            Console.WriteLine("0: sulje");
 
             string valinta = Console.ReadLine();
 
@@ -24,29 +23,39 @@ namespace Numbers
             string vastaus2a = "2";
             string vastaus3a = "3";
             int nimiLaskin = 1;
-
-
+            
             if (valinta == vastaus1a)
             {
                 Console.WriteLine("test1 = " + valinta);
                 string ydin = Console.ReadLine();
                 Console.WriteLine("Day: " + nimiLaskin);
                 Console.WriteLine(ydin);
-                string odotus = Console.ReadLine();
+                Console.WriteLine("Any: Poistu");
+                string poistu = Console.ReadLine();
             }
             //Uuden päivän luonti
 
             else if (valinta == vastaus2a)
             {
                 Console.WriteLine("test2 = " + valinta);
+                Console.WriteLine("Any: Poistu");
+                string poistu = Console.ReadLine();
             }
             //Vanhan päivän muokkaus
 
             else if (valinta == vastaus3a)
             {
                 Console.WriteLine("test3 = " + valinta);
+                Console.WriteLine("Any: Poistu");
+                string poistu = Console.ReadLine();
             }
             //Vanhan päivän lukeminen
+            
+            else if (valinta == "0")
+            {
+                count = count + 1;
+            }
         }
+
     }
 }
